6a7d3d5b42b987992f89fdf855937173b26f87eb
Will <bill.williams.tsofack.fozeing@efrei.net>
2022-12-07 15:00:06 +0100

    add country and state endpoints

--- src/main/java/com/luv2code/ecommerce/config/MyDataRestConfig.java
+++ src/main/java/com/luv2code/ecommerce/config/MyDataRestConfig.java
@@ -1,16 +1,29 @@
 package com.luv2code.ecommerce.config;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.EntityType;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+import com.luv2code.ecommerce.entity.Country;
 import com.luv2code.ecommerce.entity.Product;
 import com.luv2code.ecommerce.entity.ProductCategory;
+import com.luv2code.ecommerce.entity.State;
 
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer{
+	
+	@Autowired
+	private EntityManager entityManager;
 	
 	@Override
 	public void configureRepositoryRestConfiguration(
@@ -19,16 +32,39 @@ public class MyDataRestConfig implements RepositoryRestConfigurer{
 		HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
 		
 		// disable HTTP methods for product: PUT, POST, DELETE
-		config.getExposureConfiguration()
-				.forDomainType(Product.class)
-				.withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
-				.withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+		disabledHttpMethods(Product.class, config, theUnsupportedActions);
+		disabledHttpMethods(ProductCategory.class, config, theUnsupportedActions);
+		disabledHttpMethods(Country.class, config, theUnsupportedActions);
+		disabledHttpMethods(State.class, config, theUnsupportedActions);
 		
-		// disable HTTP methods for product-category: PUT, POST, DELETE
+		// call an internal helper method
+		exposeIds(config);
+	}
+
+	private void disabledHttpMethods(Class theClass, RepositoryRestConfiguration config, HttpMethod[] theUnsupportedActions) {
 		config.getExposureConfiguration()
-				.forDomainType(ProductCategory.class)
+				.forDomainType(theClass)
 				.withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
 				.withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
-				
+	}
+
+	private void exposeIds(RepositoryRestConfiguration config) {
+		
+		// expose entity ids
+		
+		// get a list of all entity classes from the entity manager
+		Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+		
+		// create an array of the entity types
+		List<Class> entityClasses = new ArrayList<>();
+		
+		// get the entity types for the entities
+		for (EntityType entityType: entities) {
+			entityClasses.add(entityType.getJavaType());
+		}
+		
+		// expose the entity Ids for the array of entity/ domain types
+		Class[] domainTypes = entityClasses.toArray(new Class[0]);
+		config.exposeIdsFor(domainTypes);
 	}
 }
--- src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
@@ -12,4 +12,6 @@ import com.luv2code.ecommerce.entity.Product;
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 	Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
+	
+	Page<Product> findByNameContaining(@Param("name") String name, Pageable pageable);
 }
